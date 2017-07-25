3206299eb33a87d56b5e305d511b63c7ebd3f320
James <boydston.j@gmail.com>
2017-07-25 16:33:48 -0700

    bunch of stuff

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express()
 const mustacheExpress = require('mustache-express');
+const data = require('./data.js')
+const path = require('path')
 
-app.get('/', function(req, res, next){
-  res.send('anything');
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/', function (req, res) {
+  res.render('index', data)
+});
+
+app.get("/user/:username", function(req, res, next){
+  const user = data.users.filter(function(person){
+    return person.username === req.params.username
+  })[0]
+
+  res.render("user", user)
 })
 
 app.listen(3000, function () {
   console.log('App listening on port 3000');
-})
+});
