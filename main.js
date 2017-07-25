b72c7161d1e6058edbfafcec7e2fb82cbd94d2f3
James <boydston.j@gmail.com>
2017-07-25 13:51:40 -0700

    serving

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const express = require('express');
 const app = express()
 const mustacheExpress = require('mustache-express');
+
+app.get('/', function(req, res, next){
+  res.send('anything');
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000');
+})
